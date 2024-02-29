762c747910b8f01fc4c9c26ec7e8bba3c91b3313
Zeresenay <zeresenayyaregal4@gmail.com>
2024-02-29 13:29:07 +0300

    Done

--- shell.h
+++ shell.h
@@ -1,71 +1,112 @@
-#ifndef SHELL_H
-#define SHELL_H
-#include <string.h>
-#include <stdlib.h>
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/**###### environ var ######*/
+
+extern char **environ;
+
+/**##### MACROS ######*/
+
+#define BUFSIZE 1024
+#define DELIM " \t\r\n\a"
+#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
+
+/**###### LIBS USED ######*/
+
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
 
-#define SEP_SLASH "/"
-#define SEP_EQUAL "="
-#define SEP_TPOIN ":"
-#define SEP_ESPAC " \t\r\n\a"
-#define TITLE_PATH "PATH"
-#define TITLE_DIR "PWD"
-#define TITLE_FILE "_"
+
+
+
+
+/**###### STRING FUNCTION ######*/
+
+char *_strtok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
+
+/**###### MEMORIE  MANGMENT ####*/
+
+void free_env(char **env);
+void *fill_an_array(void *a, int el, unsigned int len);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_calloc(unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void free_all(char **input, char *line);
+
+/**###### INPUT Function ######*/
+
+void prompt(void);
+void signal_to_handel(int sig);
+char *_getline(void);
+
+/** ###### Command parser and extractor ###*/
+
+int path_cmd(char **line);
+char *_getenv(char *name);
+char **parse_cmd(char *cmd);
+int handle_builtin(char **cmd, int er);
+void read_file(char *filename, char **argv);
+char *build(char *token, char *value);
+int check_builtin(char **cmd);
+void creat_envi(char **envi);
+int check_cmd(char **tokens, char *line, int count, char **argv);
+void treat_file(char *line, int counter, FILE *fd, char **argv);
+void exit_bul_for_file(char **cmd, char *line, FILE *fd);
+
+/** ####BUL FUNC #####*/
+
+void hashtag_handle(char *buff);
+int history(char *input);
+int history_dis(char **cmd, int er);
+int dis_env(char **cmd, int er);
+int change_dir(char **cmd, int er);
+int display_help(char **cmd, int er);
+int echo_bul(char **cmd, int er);
+void  exit_bul(char **cmd, char *input, char **argv, int c);
+int print_echo(char **cmd);
+
+/** ####error handle and Printer ####*/
+void print_number(unsigned int n);
+void print_number_in(int n);
+void print_error(char *line, int c, char **argv);
+void _prerror(char **argv, int c, char **cmd);
+
 
 /**
- * struct built_t - anothers commands.
- * @comand: command.
- * @func: function to executing.
+ * struct bulltin - contain bultin to handle and function to excute
+ * @command:pointer to char
+ * @fun:fun to excute when bultin true
  */
-typedef struct built_t
+
+typedef struct  bulltin
 {
-	char *comand;
-	int (*func)(char *, char **, char *, char **, int, int);
-} built;
-
-
-void func_loop(char **environ, char *argv);
-int check_string(char *str, ssize_t length);
-
-
-void no_interactive(char **environ, char *file);
-int arguments1(char *file, char **environ, char *s, int out);
-int check_string1(char *str, ssize_t length);
-
-int arguments(char *s, char *file, char **environ, int ct, int out);
-int get_command(char *s, char **arg_vec, char *fl, char **env, int ct, int out);
-
-int (*b_tns(char *s))(char *, char **, char *, char **, int, int);
-int ex(char *s, char **argv, char *file, char **environ, int ct, int out);
-int envir(char *s, char **arg_vec, char *fl, char **env, int ct, int out);
-
-int search_command(char **argv, char *file, char **environ, int ct);
-int divpath(char **argv, char *file, char **environ, int ct);
-char *_getenv(char **environ, char *path);
-char *check_exec(char *path, char *command);
-
-
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-void _strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-
-int _isdigit(char c);
-int _atoi(char *s);
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _memcpy(char *dest, char *src, unsigned int n);
-
-void _putchar(char *c);
-void newline(int number);
-void printerror(char *file, char *argv, int ct, char *message);
-void printnumber(int number);
-void printerrorex(char *file, char **argv, int ct, char *message);
+	char *command;
+	int (*fun)(char **line, int er);
+} bul_t;
 
 #endif
